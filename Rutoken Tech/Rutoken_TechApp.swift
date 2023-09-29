3602399b205fb6342dbbfcdceeb0d9dc1f961c5f
Andrei Trifonov <trifonov@rutoken.ru>
2023-09-29 09:25:24 -0400

    Initial Commit

---
+++ Rutoken Tech/Rutoken_TechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rutoken_TechApp.swift
+//  Rutoken Tech
+//
+//  Created by Андрей Трифонов on 2023-09-29.
+//
+
+import SwiftUI
+
+@main
+struct Rutoken_TechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
