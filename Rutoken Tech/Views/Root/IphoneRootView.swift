62b2bce64e4d164254f40c43f435dc36bca3a38b
Nikita Deviatykh <deviatykh@rutoken.ru>
2024-03-07 11:31:35 +0300

    Add ignoresSafeArea(.keyboard) for  disconnectedTokenView()
    Resolve: #121

--- Rutoken Tech/Views/Root/IphoneRootView.swift
+++ Rutoken Tech/Views/Root/IphoneRootView.swift
@@ -24,6 +24,7 @@ struct IphoneRootView: View {
                         CaEntryView()
                             .tabItem { Label(tab.rawValue, systemImage: tab.imageName) }
                             .tag(tab)
+                            .ignoresSafeArea(.keyboard)
                     case .about:
                         AboutAppView()
                             .tabItem { Label(tab.rawValue, systemImage: tab.imageName) }
@@ -32,6 +33,7 @@ struct IphoneRootView: View {
                         Text(tab.rawValue)
                             .tabItem { Label(tab.rawValue, systemImage: tab.imageName) }
                             .tag(tab)
+                            .ignoresSafeArea(.keyboard)
                     }
                 }
                 .background {
