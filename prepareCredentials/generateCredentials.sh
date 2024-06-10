81689b8f8c306a18586d2c32d9e2ee7802184a9d
Ivan Poderegin <poderegin@rutoken.ru>
2024-06-10 17:34:48 +0300

    Change the certificate chains in the "generateCredentials" script and update "Credentials" Resolve: #203

--- prepareCredentials/generateCredentials.sh
+++ prepareCredentials/generateCredentials.sh
@@ -17,22 +17,27 @@ $openssl_path \
   genpkey -engine rtengine -algorithm gost2012_256 -pkeyopt paramset:A -out "$credentials_dir/$sec.key"
 done
 
-# Generate certificate for the CA
-$openssl_path \
-  req -engine rtengine -utf8 -new -x509 -md_gost12_256 \
-  -utf8 -config "$script_dir/ca.conf" -days 3650 -key "$credentials_dir/ca.key" -out "$credentials_dir/ca.pem"
-
 # Generate certificate for the Root CA
 $openssl_path \
   req -engine rtengine -utf8 -new -x509 -md_gost12_256 \
   -utf8 -config "$script_dir/rootCa.conf" -days 3650 -key "$credentials_dir/rootCa.key" -out "$credentials_dir/rootCa.pem"
+
+# Generate CSR for the CA
+$openssl_path \
+  req -engine rtengine -utf8 -new -md_gost12_256 \
+  -utf8 -config "$script_dir/ca.conf" -key "$credentials_dir/ca.key" -out "$credentials_dir/ca.csr"
+
+# Sign CSR of the CA with Root CA
+$openssl_path \
+  x509 -extfile "$script_dir/ca.conf" -extensions ext -engine rtengine -req -md_gost12_256 -CA "$credentials_dir/rootCa.pem" -CAkey "$credentials_dir/rootCa.key" \
+  -in "$credentials_dir/ca.csr" -out "$credentials_dir/ca.pem" -days 3650 -CAcreateserial
 
 # Generate CSR for the Bank
 $openssl_path \
   req -engine rtengine -utf8 -new -md_gost12_256 \
   -utf8 -config "$script_dir/bank.conf" -key "$credentials_dir/bank.key" -out "$credentials_dir/bank.csr"
 
-# Sign CSR of the Bank with Root CA
+# Sign CSR of the Bank with the CA
 $openssl_path \
-  x509 -engine rtengine -req -md_gost12_256 -CA "$credentials_dir/rootCa.pem" -CAkey "$credentials_dir/rootCa.key" \
+  x509 -extfile "$script_dir/bank.conf" -extensions ext -engine rtengine -req -md_gost12_256 -CA "$credentials_dir/ca.pem" -CAkey "$credentials_dir/ca.key" \
   -in "$credentials_dir/bank.csr" -out "$credentials_dir/bank.pem" -days 3650 -CAcreateserial
