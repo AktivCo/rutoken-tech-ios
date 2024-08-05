f02aac4120579dea9e3aab8516f3792086ade9aa
Andrei Trifonov <trifonov@rutoken.ru>
2024-08-05 12:09:18 -0400

    Fix algo's name in cert card
    Resolve: #222

--- Rutoken Tech/Extensions/KeyAlgorithm+description.swift
+++ Rutoken Tech/Extensions/KeyAlgorithm+description.swift
@@ -9,7 +9,7 @@ extension Pkcs11KeyAlgorithm {
     var description: String {
         switch self {
         case .gostR3410_2012_256:
-            return "ГОСТ Р 34.10-2012 256-бит"
+            return "ГОСТ Р 34.10-2012 256 бит"
         }
     }
 }
